a4acbfac871cd84f47d586a9564aaa1f395fdd53
Thom <thomthom@live.nl>
2017-01-22 22:10:31 +0100

    First draft of collaborative filtering recommender

---
+++ 8_collaborative_filtering_recommender/run_app.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+"""
+Created on January 8, 2016
+@author: thom.hopmans
+"""
+import logging
+from data_processor import DataProcessor
+from model_fitter import ModelFitter
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class CollaborativeFiltering:
+    """Create a recommender for blogs using collaborative filtering"""
+
+    def run(self):
+        """Run the Scrape TMT articles script"""
+        behaviour_df, behaviour_matrix, df_articles, df_users = DataProcessor().generate_reading_behaviour_matrix()
+        ModelFitter(behaviour_df, behaviour_matrix, df_articles, df_users).fit_model()
+
+
+if __name__ == "__main__":
+    CollaborativeFiltering().run()
